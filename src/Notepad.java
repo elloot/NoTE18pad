17f8960bae9ed77f7e3a693eae43e27d7a51c5b6
elloot <elliot.duchek@gmail.com>
2020-03-12 11:03:04 +0100

    add basic gui

--- src/Notepad.java
+++ src/Notepad.java
@@ -8,4 +8,22 @@ import javax.swing.*;
 public class Notepad {
     private JPanel RootPane;
     private JTextPane textPane1;
+
+    public static void main(String[] args) {
+        //set look and feel
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        }
+
+        JFrame frame = new JFrame("Notepad");
+        frame.setContentPane(new Notepad().RootPane);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private JScrollBar scrollBar1;
 }
