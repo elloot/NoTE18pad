6c07e5b46a2333a36fe61f3584015a6ffd0dfd71
elloot <elliot.duchek@gmail.com>
2020-03-12 10:51:50 +0100

    Initial commit

---
+++ src/Notepad.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Class description
+ * 2020-03-12
+ * Author: Elliot Duchek
+ */
+public class Notepad {
+    private JPanel RootPane;
+    private JTextPane textPane1;
+}
